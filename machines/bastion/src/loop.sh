f9160c4dce645bfa7853c5c39892529b32d11b04
Vera0011 <contact.vera0011@gmail.com>
2026-03-16 19:37:27 +0100

    Finished bastion machine
    - Added NGINX, cron and SSH services
    - Finished configuration of all services and setting up the flags
    - Finished configuration of docker-compose.yml file
    - Added default website

--- machines/bastion/src/loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo service nginx start
-sudo service ssh start
-
-while [ 1 == 1 ]; do
-	cat -;
-	sleep 1;
-done;
--- prepare.sh
+++ prepare.sh
@@ -12,6 +12,7 @@
 > dockername.txt
 
 mapfile -t CONTAINERS < <(yq '.services | keys | .[]' ./docker-compose.yml)
+echo "[" > .env
 
 for name in "${CONTAINERS[@]}"; do
     IMAGE=$(yq ".services.$name.image" ./docker-compose.yml)
@@ -28,8 +29,10 @@ for name in "${CONTAINERS[@]}"; do
     PORTINS_STR=$(IFS=','; echo "${PORTINS[*]}")
     PORTOUTS_STR=$(IFS=','; echo "${PORTOUTS[*]}")
 
-    echo "{image: '$IMAGE', portin: '$PORTINS_STR', portout: '$PORTOUTS_STR'}" >> .env
+    echo "{image: '$IMAGE', portin: '$PORTINS_STR', portout: '$PORTOUTS_STR'}," >> .env
     echo "$name" >> dockername.txt
 done;
+
+echo "]" >> .env
 
 echo "Environment prepared. Please check .env and dockername.txt"
